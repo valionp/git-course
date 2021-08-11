9238bd95e03aee23c2f5c179b4f787a011557c6d
Petro <you@example.com>
2021-08-11 15:37:35 +0300

    com

---
+++ index.js
@@ -0,0 +1,13 @@
+const shopList = document.querySelectorAll('.shop-list')[0]
+const listOfBuys = document.querySelectorAll('.shop-list li')
+
+listOfBuys.forEach((element) => {
+	element.addEventListener('mouseover', (event) => {
+		let removeButton = event.target.querySelector('.remove')
+		removeButton.classList.toggle('visible')
+		console.log(removeButton)
+		removeButton.addEventListener('click', (event) => {
+			event.target.parentElement.remove()
+		})
+	})
+})
