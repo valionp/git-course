06f364a721bbfaecc2281ac4e0ca924e8319979b
Petro Valion <petro.valion13@gmail.com>
2021-09-01 09:41:01 +0300

    test commit

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const shopList = document.querySelectorAll('.shop-list')[0]
 const listOfBuys = document.querySelectorAll('.shop-list li')
 
 listOfBuys.forEach((element) => {
-	element.addEventListener('mouseover', (event) => {
-		let removeButton = event.target.querySelector('.remove')
-		removeButton.classList.toggle('visible')
-		console.log(removeButton)
-		removeButton.addEventListener('click', (event) => {
-			event.target.parentElement.remove()
-		})
-	})
+    element.addEventListener('mouseover', (event) => {
+        let removeButton = event.target.querySelector('.remove')
+        removeButton.classList.toggle('visible')
+        console.log(removeButton)
+        removeButton.addEventListener('click', (event) => {
+            event.target.parentElement.remove()
+        })
+    })
 })
 
 console.log('hello, GitHub!')
